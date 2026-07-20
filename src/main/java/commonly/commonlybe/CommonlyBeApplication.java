2561206789e702870b2d494f46c077daa8020232
tlgms <siheun9@gmail.com>
2026-07-20 20:13:22 +0900

    feat: Spring Boot 프로젝트 기본 세팅

---
+++ src/main/java/commonly/commonlybe/CommonlyBeApplication.java
@@ -0,0 +1,13 @@
+package commonly.commonlybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonlyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonlyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/commonly/commonlybe/CommonlyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package commonly.commonlybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonlyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
