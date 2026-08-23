7ea6cad12c130cb67a752831513807ecfcde38f8
tlgms <siheun9@gmail.com>
2026-08-23 23:32:08 +0900

    refactor: 메시지 추가

--- src/main/java/commonly/commonlybe/file/controller/dto/ColumnMapping.java
+++ src/main/java/commonly/commonlybe/file/controller/dto/ColumnMapping.java
@@ -3,7 +3,7 @@ package commonly.commonlybe.file.controller.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record ColumnMapping(
-        @NotBlank String sourceColumn,
-        @NotBlank String targetField
+        @NotBlank(message = "소스컬럼이 비어있습니다.") String sourceColumn,
+        @NotBlank(message = "목표컬럼이 비어있습니다.") String targetField
 ) {
 }
